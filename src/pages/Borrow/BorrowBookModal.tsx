f90243ce2d7278995918e964dece12370a8d9e03
muntasir010 <naeemtasir03@gmail.com>
2025-09-23 14:49:31 +0600

    update borrow form

--- src/pages/Borrow/BorrowBookModal.tsx
+++ src/pages/Borrow/BorrowBookModal.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useBorrowBookMutation } from "@/redux/api/booksCreatedApi";
 import type { IBook } from "@/type";
 import { useForm } from "react-hook-form";
@@ -37,31 +42,61 @@ export const BorrowBookModal = ({
 
       toast.success("Book borrowed successfully!");
       onClose();
-      navigate("/borrow-summary")
+      navigate("/borrow-summary");
     } catch {
       toast.error("Failed to borrow book");
     }
   };
 
   return (
+    // <Dialog open={open} onOpenChange={onClose}>
+    //   <DialogContent>
+    //     <DialogHeader>
+    //       <DialogTitle>Borrow {book.title}</DialogTitle>
+    //     </DialogHeader>
+    //     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+    //       <div>
+    //         <label>Quantity</label>
+    //         <input
+    //           type="number"
+    //           {...register("quantity", { required: true })}
+    //         />
+    //       </div>
+    //       <div>
+    //         <label>Due Date</label>
+    //         <input type="date" {...register("dueDate", { required: true })} />
+    //       </div>
+    //       <button type="submit">Submit</button>
+    //     </form>
+    //   </DialogContent>
+    // </Dialog>
     <Dialog open={open} onOpenChange={onClose}>
-      <DialogContent>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader className="text-2xl">Borrow Book</DialogHeader>
         <DialogHeader>
-          <DialogTitle>Borrow {book.title}</DialogTitle>
+          <DialogTitle>Name: {book.title}</DialogTitle>
         </DialogHeader>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-          <div>
-            <label>Quantity</label>
-            <input
-              type="number"
-              {...register("quantity", { required: true })}
-            />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid gap-4">
+            <div className="grid gap-3">
+              <Label htmlFor="name-1">Quantity</Label>
+              <Input
+                {...register("quantity", { required: true })}
+                type="number"
+              />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="username-1">Due Date</Label>
+              <Input type="date" {...register("dueDate", { required: true })} />
+            </div>
           </div>
-          <div>
-            <label>Due Date</label>
-            <input type="date" {...register("dueDate", { required: true })} />
-          </div>
-          <button type="submit">Submit</button>
+
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Submit</Button>
+          </DialogFooter>
         </form>
       </DialogContent>
     </Dialog>
